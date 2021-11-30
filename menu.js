39064782faf66a40d8e87d5eff941732b8fd1644
raffmares <raff@internetofelephants.com>
2021-11-30 16:09:20 +0000

    Improved code

--- menu.js
+++ menu.js
@@ -1,21 +1,22 @@
+const menu = document.getElementById('pages');
+const content = document.getElementById('content');
+
 // toggle between showing and hiding page links when clicking on 'hamburger' menu div and menu items
 function toggleMenu() {
-  var x = document.getElementById('pages');
-  if (x.style.maxHeight === '300px') {
-    x.style.maxHeight = '0';
-    x.removeEventListener('click', closeMenu);
-    document.getElementById('content').removeEventListener('click', closeMenu);
+  if (menu.style.maxHeight === '300px') {
+    menu.style.maxHeight = '0';
+    menu.removeEventListener('click', closeMenu);
+    content.removeEventListener('click', closeMenu);
   } else {
-    x.style.maxHeight = '300px';
-    x.addEventListener('click', closeMenu);
-    document.getElementById('content').addEventListener('click', closeMenu);
+    menu.style.maxHeight = '300px';
+    menu.addEventListener('click', closeMenu);
+    content.addEventListener('click', closeMenu);
   }
 }
 
-// close menu when clicking on menu iteme or page content
+// close menu when clicking on menu item or page content
 function closeMenu() {
-  var x = document.getElementById('pages');
-  x.style.maxHeight = '0';
-  x.removeEventListener('click', closeMenu);
-  document.getElementById('content').removeEventListener('click', closeMenu);
+  menu.style.maxHeight = '0';
+  menu.removeEventListener('click', closeMenu);
+  content.removeEventListener('click', closeMenu);
 }
